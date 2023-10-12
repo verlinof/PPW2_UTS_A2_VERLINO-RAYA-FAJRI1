03b8b1cd5edd52a2af06b3773d7ebaf8c81f7257
verlinof <ajikoko.zx@gmail.com>
2023-10-12 08:43:40 +0700

    Fixing Tampilan

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -9,8 +9,10 @@
 </head>
 <body>
     @include('components.navbar')
-
-    @yield('root')
+    
+    <div class="my-3">
+        @yield('root')
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
